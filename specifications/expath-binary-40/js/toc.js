3a268fe0f65337b55ed1bff2d8e662696e4ab567
ndw <ndw@nwalsh.com>
2025-03-04 17:33:57 +0000

    Deploying to gh-pages from @ qt4cg/qtspecs@a741073b96b49ca0af63c18e8833dadfbe342037 🚀

--- specifications/expath-binary-40/js/toc.js
+++ specifications/expath-binary-40/js/toc.js
@@ -1,38 +1,4 @@
 (function() {
-  const updateDelta = function(target) {
-    if (target.classList.contains("expanded")) {
-      return;
-    }
-
-    let anchor = target;
-    while (anchor && anchor.tagName !== "A") {
-      anchor = anchor.previousSibling;
-    }
-    if (anchor && (anchor.querySelector("span.toc-chg")
-                   || anchor.querySelector("span.toc-new"))) {
-      return;
-    }
-
-    let delta = null;
-    let details = target;
-    while (details && details.tagName !== "DETAILS") {
-      details = details.parentNode;
-    }
-    if (details) {
-      delta = (details.querySelector("span.toc-chg")
-               || details.querySelector("span.toc-new"))
-    }
-
-    // We're assuming innerHTML is a single text node...
-    let inner = `${target.innerHTML}`;
-    inner = inner.substring(inner.length - 1)
-    if (delta) {
-      inner = "★" + inner;
-    }
-
-    target.innerHTML = inner;
-  }
-
   const updateToc = function(open) {
     document.querySelectorAll(".toc details").forEach(details => {
       details.open = open
@@ -47,28 +13,22 @@
         span.classList.add("collapsed")
         span.innerHTML = "\u2009▶"
       }
-      updateDelta(span);
     });
   }
 
   window.addEventListener("load", () => {
     document.querySelectorAll(".exptoc").forEach(span => {
-      updateDelta(span)
-    });
-
-    document.querySelectorAll(".toc details summary").forEach(summary => {
-      summary.addEventListener("click", (event) => {
-        let target = event.target.querySelector(".exptoc")
+      span.addEventListener("click", (event) => {
+        let target = event.target;
         if (target.classList.contains("collapsed")) {
-          target.classList.remove("collapsed");
-          target.classList.add("expanded");
-          target.innerHTML = `\u2009▼`;
+          target.classList.remove("collapsed")
+          target.classList.add("expanded")
+          target.innerHTML = "\u2009▼"
         } else {
-          target.classList.remove("expanded");
-          target.classList.add("collapsed");
-          target.innerHTML = `\u2009▶`;
+          target.classList.remove("expanded")
+          target.classList.add("collapsed")
+          target.innerHTML = "\u2009▶"
         }
-        updateDelta(target);
       });
     });
 
