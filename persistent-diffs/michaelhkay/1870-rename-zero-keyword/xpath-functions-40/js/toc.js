e2d413ddfea7a97579f25bdc1563baee1e88286c
Norman Walsh <ndw@nwalsh.com>
2026-03-13 13:19:17 +0000

    First attempt at 'persistent diff' pages

---
+++ persistent-diffs/michaelhkay/1870-rename-zero-keyword/xpath-functions-40/js/toc.js
@@ -0,0 +1,93 @@
+(function() {
+  const updateDelta = function(target) {
+    if (target.classList.contains("expanded")) {
+      return;
+    }
+
+    let anchor = target;
+    while (anchor && anchor.tagName !== "A") {
+      anchor = anchor.previousSibling;
+    }
+    if (anchor && (anchor.querySelector("span.toc-chg")
+                   || anchor.querySelector("span.toc-new"))) {
+      return;
+    }
+
+    let delta = null;
+    let details = target;
+    while (details && details.tagName !== "DETAILS") {
+      details = details.parentNode;
+    }
+    if (details) {
+      delta = (details.querySelector("span.toc-chg")
+               || details.querySelector("span.toc-new"))
+    }
+
+    // We're assuming innerHTML is a single text node...
+    let inner = `${target.innerHTML}`;
+    inner = inner.substring(inner.length - 1)
+    if (delta) {
+      inner = "★" + inner;
+    }
+
+    target.innerHTML = inner;
+  }
+
+  const updateToc = function(open) {
+    document.querySelectorAll(".toc details").forEach(details => {
+      details.open = open
+    });
+    document.querySelectorAll(".exptoc").forEach(span => {
+      if (open) {
+        span.classList.remove("collapsed")
+        span.classList.add("expanded")
+        span.innerHTML = "\u2009▼"
+      } else {
+        span.classList.remove("expanded")
+        span.classList.add("collapsed")
+        span.innerHTML = "\u2009▶"
+      }
+      updateDelta(span);
+    });
+  }
+
+  window.addEventListener("load", () => {
+    document.querySelectorAll(".exptoc").forEach(span => {
+      updateDelta(span)
+    });
+
+    document.querySelectorAll(".toc details summary").forEach(summary => {
+      summary.addEventListener("click", (event) => {
+        let target = event.target.querySelector(".exptoc")
+        if (target.classList.contains("collapsed")) {
+          target.classList.remove("collapsed");
+          target.classList.add("expanded");
+          target.innerHTML = `\u2009▼`;
+        } else {
+          target.classList.remove("expanded");
+          target.classList.add("collapsed");
+          target.innerHTML = `\u2009▶`;
+        }
+        updateDelta(target);
+      });
+    });
+
+    let expandAll = document.querySelector(".expalltoc")
+    if (expandAll) {
+      expandAll.addEventListener("click", (event) => {
+        let target = event.target;
+        if (target.classList.contains("collapsed")) {
+          target.classList.remove("collapsed")
+          target.classList.add("expanded")
+          target.innerHTML = "\u2009▼"
+          updateToc(true)
+        } else {
+          target.classList.remove("expanded")
+          target.classList.add("collapsed")
+          target.innerHTML = "\u2009▶"
+          updateToc(false)
+        }
+      });
+    }
+  });
+})();
